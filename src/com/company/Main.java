25acd196f9a86afb4008109dee99a4f36503e500
Ali Elbashir <alielbashiir@gmail.com>
2020-11-15 01:14:03 +0300

    Revert "Add messi test"
    This reverts commit 5befc11e

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,12 +1,9 @@
 package com.company;
+
 public class Main {
 
     public static void main(String[] args) {
 	// lkdsjfkldsjkfl
         System.out.println("deneme 1 2 3");
-        sporcu messi = new sporcu();
-        messi.yas = 26;
-        messi.ad = "lionel";
-        System.out.println(messi.yas + " yasinda, " + messi.ad);
     }
 }
--- src/com/company/sporcu.java
+++
@@ -1,6 +0,0 @@
-package com.company;
-
-public class sporcu {
-    int yas;
-    String ad;
-}
