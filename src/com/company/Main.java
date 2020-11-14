957b64bc99311b449f50ed006bc5482a74fb12b9
Ali Elbashir <alielbashiir@gmail.com>
2020-11-14 18:01:55 +0300

    Add test print

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	// lkdsjfkldsjkfl
+        System.out.println("deneme 1 2 3");
     }
 }
