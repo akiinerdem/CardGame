68b0e303bba4af788104f12b0286effb2aa9b158
Ali Elbashir <alielbashiir@gmail.com>
2020-11-15 02:26:48 +0300

    Add KartSec method to Bilgisayar

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -16,7 +16,7 @@ public class Main {
         System.out.println("deneme 1 2 3");
         Bilgisayar lenovo = new Bilgisayar();
         lenovo.BilinenBilgisayar(52, "Lenovo G50", 24, kartlar);
-        System.out.println(lenovo.oyuncuID + ". " + lenovo.oyuncuAdi + " " + lenovo.Skor);
+        System.out.println(lenovo.oyuncuID + ". " + lenovo.oyuncuAdi + " " + lenovo.Skor + " puan");
         System.out.println();
 
     }
