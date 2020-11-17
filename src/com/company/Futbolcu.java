8c4a1954f5d0aec78d2bec25da2da57db7ac9897
akiinerdem <akiinerdem@gmail.com>
2020-11-18 00:50:29 +0300

    18.11.2020

--- src/com/company/Futbolcu.java
+++ src/com/company/Futbolcu.java
@@ -7,6 +7,10 @@ public class Futbolcu extends Sporcu {
     private int serbestVurus;
     private int kaleciKarsiKarsiya;
 
+    public Futbolcu(){
+
+    }
+
     public Futbolcu(String futbolcuAdi,String futbolcuTakim,int penalti,int serbestVurus,int kaleciKarsiKarsiya) {
         this.futbolcuAdi = futbolcuAdi;
         this.futbolcuTakim= futbolcuTakim;
@@ -19,9 +23,41 @@ public class Futbolcu extends Sporcu {
     }
 
 
- 
+    Futbolcu LionelMessi= new Futbolcu();
+    Futbolcu CristianoRonalo = new Futbolcu();
+    Futbolcu TonyKross= new Futbolcu();
+    Futbolcu MohammedSalah= new Futbolcu();
+    Futbolcu RobertLewandowski= new Futbolcu();
+    Futbolcu MarcoReus= new Futbolcu();
+    Futbolcu Neymar= new Futbolcu();
+    Futbolcu KarimBenzema= new Futbolcu();
 
 
 
 
+
+
+    public int getSerbestVurus() {
+        return serbestVurus;
+    }
+
+    public void setSerbestVurus(int serbestVurus) {
+        this.serbestVurus = serbestVurus;
+    }
+
+    public int getPenalti() {
+        return penalti;
+    }
+
+    public void setPenalti(int penalti) {
+        this.penalti = penalti;
+    }
+
+    public int getKaleciKarsiKarsiya() {
+        return kaleciKarsiKarsiya;
+    }
+
+    public void setKaleciKarsiKarsiya(int kaleciKarsiKarsiya) {
+        this.kaleciKarsiKarsiya = kaleciKarsiKarsiya;
+    }
 }
