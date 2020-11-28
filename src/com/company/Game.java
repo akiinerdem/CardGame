a8400bb3a3149870653c6deb1ab62a9e62b7ec11
akiinerdem <akiinerdem@gmail.com>
2020-11-28 22:18:50 +0300

    arayüz deneme

---
+++ src/com/company/Game.java
@@ -0,0 +1,26 @@
+package com.company;
+
+import javax.swing.*;
+
+public class Game extends JFrame {
+    private JButton button1;
+    private JButton button2;
+    private JButton button3;
+    private JButton button4;
+    private JButton button5;
+    private JButton button6;
+    private JButton button7;
+    private JButton button8;
+    private JButton button9;
+    private JButton button10;
+    private JButton button11;
+    private JButton button12;
+    private JButton button13;
+    private JButton button14;
+    private JButton button15;
+    private JButton button16;
+    private JButton button17;
+    private JButton button18;
+    private JTextField textField1;
+    private JTextField textField2;
+}
